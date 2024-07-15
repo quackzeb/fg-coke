6745091bd72960d65e9d2e30e0e29bc08e170dd6
quackz <131069065+quackzeb@users.noreply.github.com>
2024-07-15 14:14:34 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,3 +6,17 @@ Config.Coke = {
     Amount = 1, -- The Amount of coke each harvest
     ItemFarm = "coca_leaf" -- Item given after each harvest
 }
+
+Config.CokePlant = 'prop_plant_01a' -- The model of the coke plants
+
+Config.CokePlantLoc = { -- The locations of the cokeplants to spawn at
+    vector3(-2164.42, 5177.58, 14.93),
+    vector3(-2178.75, 5177.67, 15.7),
+    vector3(-2190.01, 5177.04, 15.3),
+    vector3(-2188.29, 5191.62, 18.12),
+    vector3(-2158.44, 5171.27, 13.76),
+    vector3(-2161.61, 5161.26, 12.93),
+    vector3(-2193.84, 5148.27, 11.03),
+    vector3(-2199.09, 5141.53, 12.05),
+    vector3(-2205.03, 5140.11, 12.12)
+}
